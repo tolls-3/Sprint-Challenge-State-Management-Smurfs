b881b2add3e26f12b3a2f9bcb03d5a34e8087d6b
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-10-11 08:49:57 +0100

    created action for fetching smurf in action creators

--- smurfs/src/state/actionCreators.js
+++ smurfs/src/state/actionCreators.js
@@ -1,1 +1,25 @@
-import * as types from './actionTypes';
+import axios from "axios";
+import * as types from "./actionTypes";
+
+const smurfApi = "http://localhost:3333/smurfs";
+
+export function fetchSmurf() {
+    dispatch({
+        type:types.FETCH_SMURF_START, 
+        payload:res.data
+    })
+  return function(dispatch) {
+      axios
+      .get(smurfApi)
+      .then(res=>
+        dispatch({
+            type:types.FETCH_SMURF_SUCCESS, 
+            payload:res.data
+        })
+        )
+        .catch(err=>dispatch({
+            type:types.FETCH_SMURF_FAILURE,
+            payload:err
+        }))
+  };
+}
